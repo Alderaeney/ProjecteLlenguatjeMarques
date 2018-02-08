9849bca6cf1e7b41ae446c0cce4087d44f9e35e3
Alderaeney <alderaeney@protonmail.com>
2018-02-08 16:50:30 +0100

    Base creada

--- include/contact.php
+++ include/contact.php
@@ -1,45 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>No Name Prod.</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/jquery.min.js"></script>
+    <title>SuperTuxKart</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.min.js"></script>
 </head>
-
 <body>
     <div id="pagina">
         <header>
-            <h1 class="head">
-                No Name Productions >_
-            </h1>
-            <p class="head">
-                Game developing company
-            </p>
+            
         </header>
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">WebSiteName</a>
+                    <a class="navbar-brand" href="../index.html">No Name Prod.</a>
                 </div>
                 <ul class="nav navbar-nav">
                     <li class="active">
-                        <a href="#">Home</a>
+                        <a href="../index.html">Home</a>
                     </li>
                     <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="./html/proyectos.html">Projects
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Projects
                             <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu">
                             <li>
-                                <a href="./html/supertux.html">SuperTuxKart</a>
+                                <a href="../html/supertux.html">SuperTuxKart</a>
                             </li>
                             <li>
-                                <a href="#"></a>
+                                <a href="#">Page 1-2</a>
                             </li>
                             <li>
                                 <a href="#">Page 1-3</a>
@@ -47,10 +40,10 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#">Page 2</a>
+                        <a href="#">Contact</a>
                     </li>
                     <li>
-                        <a href="#">Page 3</a>
+                        <a href="../html/media.html">Media</a>
                     </li>
                 </ul>
             </div>
@@ -64,7 +57,5 @@
 
         </footer>
     </div>
-
 </body>
-
 </html>
